e87a4bff4c9b7f80ddc8be8660392448726b0e5f
strfunctionk <comzun01@gmail.com>
2026-03-05 03:18:41 +0900

    feat: Sidebar 컴포넌트 추가 및 GNB 연동
    - Sidebar, Sidebar.stories 추가
    
    - GNB에 Sidebar 연동 (isSidebarOpen 상태 관리)
    
    - MenuButton에 isOpen/onClick props 추가
    
    - Gnb에 'use client' 적용
    
    - Gnb.stories를 Wrapper 컴포넌트 기반으로 리팩토링
    
    IssueNum #3

--- src/layouts/gnb/Gnb.tsx
+++ src/layouts/gnb/Gnb.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import AuthButton from "./components/AuthButton";
 import LogoButton from "./components/LogoButton";
@@ -6,11 +9,14 @@ import NotificationButton from "./components/NotificationButton";
 import ProjectRegisterButton from "./components/ProjectRegisterButton";
 import TextButton from "./components/TextButton";
 import MenuButton from "./components/MenuButton";
+import Sidebar from "@/layouts/sidebar/Sidebar";
 
 // TODO : Nav 메뉴는 추후에 라우팅이 확정되면 Link 컴포넌트로 변경
 const Gnb = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
-    <header className="flex items-center bg-ui-bg w-full h-80pxr px-24pxr phone:px-48pxr tablet:px-80pxr justify-between">
+    <>
+      <header className="flex items-center bg-ui-bg w-full h-80pxr px-24pxr phone:px-48pxr tablet:px-80pxr justify-between">
       {/* 좌측 로고 및 Nav 메뉴 영역 */}
       <div className="flex items-center gap-36pxr">
         {/* 로고 */}
@@ -41,9 +47,17 @@ const Gnb = () => {
           <TextButton href="" label="내 정보" />
         </SignedIn>
         {/* 모바일 환경일 때 메뉴 버튼 */}
-        <MenuButton className="tablet:hidden" />
+        <MenuButton
+          isOpen={isSidebarOpen}
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="tablet:hidden"
+        />
       </div>
     </header>
+
+      {/* 모바일 사이드바 */}
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+    </>
   );
 };
 
